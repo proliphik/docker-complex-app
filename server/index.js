6cfe1ebb492f45d6bd0dff0d8ad169e7e5ba0b18
proliphik <jerryscourses@gmail.com>
2024-12-14 14:30:01 -0500

    get rid of default value

--- server/index.js
+++ server/index.js
@@ -63,7 +63,7 @@ app.get('/values/current', async (req, res) => {
 });
 
 app.post('/values', async (req, res) => {
-  const index = req.body.index || 25;
+  const index = req.body.index;
 
   if (parseInt(index) > 40) {
     return res.status(422).send('Index too high');
